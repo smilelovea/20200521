568c07adbc61b6c4787aac80c160958ec4fb5bce
smilelovea <smilelove_a@naver.com>
2020-06-17 14:48:12 +0900

    save

--- lol/src/App.js
+++ lol/src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        axios
+            .get("https://kr.api.riotgames.com/tft/league/v1/challenger?api_key=RGAPI-1518764a-d149-4ec5-a36c-b39cfd0a5f5c")
+            .then(response => console.log(response));
+    });
+    
+    return <div></div>;
+}
+
+
+
 
 
 export default App;
