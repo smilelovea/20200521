c43f63618aca54f359613905ec3f2f684a678416
jongfeel <DeveloperFEEL@gmail.com>
2020-07-11 00:04:23 +0900

    React render code modified: for loop, index condition

--- lol/src/chessboard.js
+++ lol/src/chessboard.js
@@ -6,58 +6,19 @@ import './chessboard.css';
 
 function chessboard() {
 return (
-<div className="container">
-    <div className="holder" ondragover = "onDragOver(event);" ondrop = "onDrop(event);">
-        <div className="box" 
-            draggable="true" 
-            id="1"
-            ondragstart = "onDragStart(event);"> 
-        </div>
+    <div className="container">
+        {Array(4).fill().map((_,i) => (
+            <div className="holder" onDragOver = {event => event.preventDefault()} onDrop = {event => onDrop_handler(event)} key={i}>
+                {i <= 1 ? <div className="box" draggable="true" id={i} onDragStart = {event => event.dataTransfer.setData('text/plain', event.target.id)}></div> : ''}
+            </div>
+        ))}
     </div>
-<div className="holder" ondragover = "onDragOver(event);" ondrop = "onDrop(event);"></div>
-<div className="holder" ondragover = "onDragOver(event);" ondrop = "onDrop(event);"></div>
-<div className="holder" ondragover = "onDragOver(event);" ondrop = "onDrop(event);"></div>
-</div>
-) }
+)}
 
-/* var dragged;
-
-document.addEventListener("drag", function(event){});
-
-document.addEventListener("dragstart",function(event) {
-    dragged = event.target;
-});
-
-document.addEventListener("dragover",function(event){
+ const onDrop_handler = event => {
     event.preventDefault();
-});
-
-document.addEventListener("drop",function(event){
-    event.preventDefault();
-    if (event.target.className == "holder") {
-        dragged.parentNode.removeChild(dragged);
-        event.target.appendChild(dragged);
-    }
-}); */
-
- 
- function onDragOver(event){
-     event.preventDefault();
-     
- }
-
- function onDragStart(event) {
-     event.dataTransfer.setData('text/plain', event.target.id);
-      
- }
-
-
- 
- function onDrop(event){
-    event.preventDefault();
-    const id = event.dataTransfer.getData('text'); 
+    const id = event.dataTransfer.getData('text');
     event.target.appendChild(document.getElementById(id));
-      
  }
 
 export default chessboard; 
