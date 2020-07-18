93852bbe6942d1c4c0b57ba5104af042383afe85
smilelovea <smilelove_a@naver.com>
2020-07-18 15:03:33 +0900

    name

--- lol/src/chessboard.js
+++ lol/src/chessboard.js
@@ -7,7 +7,7 @@ import './chessboard.css';
 function chessboard() {
 return (
     <div className="container">
-        {Array(4).fill().map((_,i) => (
+        {Array(28).fill().map((_,i) => (
             <div className="holder" onDragOver = {event => event.preventDefault()} onDrop = {event => onDrop_handler(event)} key={i}>
                 {i <= 1 ? <div className="box" draggable="true" id={i} onDragStart = {event => event.dataTransfer.setData('text/plain', event.target.id)}></div> : ''}
             </div>
@@ -15,10 +15,10 @@ return (
     </div>
 )}
 
- const onDrop_handler = event => {
-    event.preventDefault();
-    const id = event.dataTransfer.getData('text');
-    event.target.append(document.getElementById(id));
+ const onDrop_handler = dropevent => {
+    dropevent.preventDefault();
+    const data = dropevent.dataTransfer.getData('text');
+    dropevent.target.appendChild(document.getElementById(data));
  };
 
  
