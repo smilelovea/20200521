216e1a4dcd8c5df55ca20e79fc905ded5b7eb65e
smilelove <smilelove_a@naver.com>
2020-07-19 11:44:50 +0900

    수정

--- lol/src/chessboard.js
+++ lol/src/chessboard.js
@@ -9,16 +9,18 @@ return (
     <div className="container">
         {Array(28).fill().map((_,i) => (
             <div className="holder" onDragOver = {event => event.preventDefault()} onDrop = {event => onDrop_handler(event)} key={i}>
-                {i <= 1 ? <div className="box" draggable="true" id={i} onDragStart = {event => event.dataTransfer.setData('text/plain', event.target.id)}></div> : ''}
+                {i <= 1 ? <div className="box" draggable="true" id={i} onDragStart = {event => event.dataTransfer.setData('text', event.target.id)}></div> : ''}
             </div>
         ))}
     </div>
 )}
 
+
  const onDrop_handler = dropevent => {
     dropevent.preventDefault();
     const data = dropevent.dataTransfer.getData('text');
     dropevent.target.appendChild(document.getElementById(data));
+    
  };
 
  
