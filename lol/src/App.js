9503bb3fafa3085de468afbbd9357fc457a54018
smilelove <smilelove_a@naver.com>
2020-08-08 16:50:36 +0900

    key

--- lol/src/App.js
+++ lol/src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     const [posts, setPosts] = useState({})
     
     async function fetchData(){ 
-        const res = await fetch ("https://kr.api.riotgames.com/tft/league/v1/entries/DIAMOND/I?page=1&api_key=RGAPI-9e164cdc-6b6f-4108-a974-f82c83c8fff6")
+        const res = await fetch ("https://kr.api.riotgames.com/tft/league/v1/entries/DIAMOND/I?page=1&api_key=RGAPI-b59b58ea-8375-497e-a1ea-a30a496a710c")
         const data = await res.json()
         setPosts(data)
 }
