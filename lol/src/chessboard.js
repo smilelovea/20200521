724fa2d314c857160d4fa92c1b8bd568b0878b08
smilelove <smilelove_a@naver.com>
2020-08-18 22:23:35 +0900

    2223

--- lol/src/chessboard.js
+++ lol/src/chessboard.js
@@ -28,11 +28,15 @@ const onDrop_prevent = event => {
     const data = event.dataTransfer.getData('text');
     console.log(data);
     console.log(event.currentTarget.firstElementChild); // 겹쳐진 box중 아래에 있는 box(원래 있던 box)
-    console.log(event.currentTarget);
+    console.log(data.parentNode);
+
+    
 
     if (data) {
         event.target.append(document.getElementById(data))
     }
+
+
  };
 
 
