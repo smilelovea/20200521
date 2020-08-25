5c7c991747e6e16f9a32019c1408c6a8eb468638
smilelove <smilelove_a@naver.com>
2020-08-25 22:56:16 +0900

    backgroundcolor

--- lol/src/chessboard.js
+++ lol/src/chessboard.js
@@ -8,34 +8,34 @@ function chessboard() {
 return (
     <div className="container">
         {Array(28).fill().map((_,i) => (
-            <div className="holder" onDragOver = {event => event.preventDefault()} 
-                 onDrop = {event => onDrop_handler(event)} key={i}  >
+            <div className="holder" onDragOver = {event => event.preventDefault()} dragstart = {dragStart} dragend = {dragEnd}
+                 onDrop = {onDrop_handler} key={i} >
                 {i <= 1 ? <div className="box" draggable="true" onDrop = {onDrop_prevent} 
-                id={i} onDragStart = {event => event.dataTransfer.setData('text', event.target.id)} 
-                 /*onDragEnter = {dragEnter_handler(holder)} onDragLeave = {dragLeave_handler(holder)}*/> </div> : ''}
+                id={i} onDragStart = {event => event.dataTransfer.setData('text', event.target.id)} /> : ''}
             </div>
         ))}
     </div>
  
 )}
 
-const onDrop_prevent = event => {
+
+function onDrop_prevent(event){
     event.preventDefault();
     event.stopPropagation();
 }
 
- const onDrop_handler = event => {
+function onDrop_handler(event) {
     event.preventDefault();
     const data = event.dataTransfer.getData('text');
     console.log(data);
     console.log(event.currentTarget.firstElementChild); // 겹쳐진 box중 아래에 있는 box(원래 있던 box)
-    console.log(data.parentNode);
-
 
     if (data) {
         event.target.append(document.getElementById(data))
-    }
-};
+    } 
+;}
+
+
 
 const holder = document.getElementsByClassName("holder");
 console.log(holder); //htmlCollection
@@ -44,17 +44,15 @@ console.log(document.querySelectorAll('box')); // NodeList[]
 
 
 
-/* 
-const dragEnter_handler = event => {
-    event.classList.add('over');
+
+
+function dragStart(){
+    holder.style.backgroundColor = "red";
 }
 
-const dragLeave_handler = event => {
-    event.classList.remove('over');
+function dragEnd(){
+    holder.style.backgroundColor = "white";
 }
-*/ 
-
-
 
 
 
