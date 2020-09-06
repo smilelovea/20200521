cde70eaf4ef415300ab9002d3883675f69cdaa86
smilelove <smilelove_a@naver.com>
2020-09-06 21:49:34 +0900

    replacechild

--- lol/src/chessboard.js
+++ lol/src/chessboard.js
@@ -10,7 +10,7 @@ return (
         {Array(28).fill().map((_,i) => (
             <div className="holder" onDragOver = {event => event.preventDefault()} onDragStart = {dragStart} onDragEnd = {dragEnd}
                  onDrop = {onDrop_handler} key={i} >
-                {i <= 1 ? <div className="box" draggable="true" onDrop = {onDrop_prevent} 
+                {i <= 2 ? <div className="box" draggable="true" onDrop = {onDrop_prevent} 
                 id={i} onDragStart = {event => event.dataTransfer.setData('text', event.target.id)} /> : ''}
             </div>
         ))}
@@ -22,6 +22,7 @@ return (
 function onDrop_prevent(event){
     event.preventDefault();
     event.stopPropagation();
+    
 } 
 
 function onDrop_handler(event) {
@@ -30,22 +31,22 @@ function onDrop_handler(event) {
     console.log(data);
     console.log(event.currentTarget.firstElementChild); // 겹쳐진 box중 아래에 있는 box(원래 있던 box)
     
-    const boxClass = document.getElementsByClassName('box');
-    console.log(boxClass);
-    
-
     if (data) {
         event.target.append(document.getElementById(data));  
         
     }
-    
+
+    const boxClass = document.getElementsByClassName('box');
+    console.log(boxClass);
+    const boxFirstChild = event.currentTarget.firstElementChild;
     if (data) {
-        const boxFirstChild = event.currentTarget.firstElementChild;
         console.log(boxFirstChild); 
         const boxLastChild = event.currentTarget.lastChild;
         console.log(event.currentTarget.lastChild);
-        event.target.insertBefore(boxLastChild,boxFirstChild);
-    }
+        event.target.replaceChild(boxLastChild,boxFirstChild);
+        event.target.appendChild(boxFirstChild);
+       
+    } 
 
 ;}
 
@@ -54,14 +55,15 @@ function onDrop_handler(event) {
 const holders = document.getElementsByClassName("holder");
 
 function dragStart(event){
-    // for (let i=0; i<holders.length; i++) {
-    //     //console.log(holders[i]);
+    //for (let i=0; i<holders.length; i++) {
+    //    // console.log(holders[i]);
     //     if (holders[i].firstChild == null) {
-    //         //console.log(holders[i]);
+    //         console.log(holders[i]);
     //         holders[i].style.backgroundColor = "red";
     //     }
     // }
-    Array.from(holders).filter(element => element.firstChild == null).forEach(element => element.style.backgroundColor = "gray");
+    Array.from(holders).filter(element => element.firstChild == null).forEach(element => element.style.backgroundColor = "#cccccc");
+    
 }
 
 function dragEnd(event){
