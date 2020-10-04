242b9381c799c48ce678bee71c0ae451ae28f426
smilelovea <smilelove_a@naver.com>
2020-10-04 13:58:09 +0900

    add champbox

--- lol/src/chessboard.js
+++ lol/src/chessboard.js
@@ -1,90 +1,105 @@
-  
-import React, { useState } from 'react';
+import React, { useState } from "react";
 //import ReactDOM, { render } from 'react-dom';
 //import App from './App';
-import './chessboard.css';
-
+import "./chessboard.css";
 
 function Chessboard() {
-return (
+  return (
     <div className="container">
-        {Array(28).fill().map((_,i) => (
-            <div className="holder" onDragOver = {event => event.preventDefault()} onDragStart = {dragStart} onDragEnd = {dragEnd}
-                onDrop = {onDrop_handler} onDragEnter = {dragEnter} onDragLeave = {dragLeave} key={i} >
-                {i <= 2 ? <div className="box" draggable="true" onDrop = {onDrop_prevent} 
-                id={i} onDragStart = {onDragStart_box} /> : ''}
+      <div className="chessboard">
+        {Array(28)
+          .fill()
+          .map((_, i) => (
+            <div
+              className="holder"
+              onDragOver={(event) => event.preventDefault()}
+              onDragStart={dragStart}
+              onDragEnd={dragEnd}
+              onDrop={onDrop_handler}
+              onDragEnter={dragEnter}
+              onDragLeave={dragLeave}
+              key={i}
+            >
+              {i <= 2 ? (
+                <div
+                  className="box"
+                  draggable="true"
+                  onDrop={onDrop_prevent}
+                  id={i}
+                  onDragStart={onDragStart_box}
+                />
+              ) : (
+                ""
+              )}
             </div>
-        ))}
+          ))}
+      </div>
+
+      <div className="champbox">hi</div>
     </div>
- 
-)}
-
-
-function onDragStart_box(event){
-    event.dataTransfer.setData('text', event.target.id);
-
+  );
 }
 
+function onDragStart_box(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
 
-function onDrop_prevent(event){
-    event.preventDefault();
-    event.stopPropagation();
-    
-} 
+function onDrop_prevent(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
 
 function onDrop_handler(event) {
-    event.preventDefault();
-    const data = event.dataTransfer.getData('text');
-   
-    if (data) {
-        event.target.append(document.getElementById(data));  
-    }
+  event.preventDefault();
+  const data = event.dataTransfer.getData("text");
 
-    const boxClass = document.getElementsByClassName('box');
-   
-    const boxFirstChild = event.currentTarget.firstElementChild;
-    if (data) {        
-        const boxLastChild = event.currentTarget.lastChild;
-        event.target.replaceChild(boxLastChild,boxFirstChild);
-        event.target.appendChild(boxFirstChild);       
-    } 
-    
-;}
+  if (data) {
+    event.target.append(document.getElementById(data));
+  }
 
+  const boxClass = document.getElementsByClassName("box");
 
+  const boxFirstChild = event.currentTarget.firstElementChild;
+  if (data) {
+    const boxLastChild = event.currentTarget.lastChild;
+    event.target.replaceChild(boxLastChild, boxFirstChild);
+    event.target.appendChild(boxFirstChild);
+  }
+}
 
 const holders = document.getElementsByClassName("holder");
 
-function dragStart(event){
-    //for (let i=0; i<holders.length; i++) {
-    //    // console.log(holders[i]);
-    //     if (holders[i].firstChild == null) {
-    //         console.log(holders[i]);
-    //         holders[i].style.backgroundColor = "red";
-    //     }
-    // }
-    Array.from(holders).filter(element => element.firstChild == null).forEach(element => element.style.backgroundColor = "#cccccc");
-    console.log(event.currentTarget);
-    event.currentTarget.classList.remove('over');
+function dragStart(event) {
+  //for (let i=0; i<holders.length; i++) {
+  //    // console.log(holders[i]);
+  //     if (holders[i].firstChild == null) {
+  //         console.log(holders[i]);
+  //         holders[i].style.backgroundColor = "red";
+  //     }
+  // }
+  Array.from(holders)
+    .filter((element) => element.firstChild == null)
+    .forEach((element) => (element.style.backgroundColor = "#cccccc"));
+  console.log(event.currentTarget);
+  event.currentTarget.classList.remove("over");
 }
 
+function dragEnd(event) {
+  Array.from(holders)
+    .filter((element) => element.firstChild == null)
+    .forEach((element) => (element.style.backgroundColor = "white"));
+  // console.log(event.currentTarget);
+}
 
+function dragEnter(event) {
+  event.preventDefault();
+  event.target.classList.add("over");
+}
 
-function dragEnd(event){
-    Array.from(holders).filter(element => element.firstChild == null).forEach(element => element.style.backgroundColor = "white");
-   // console.log(event.currentTarget);
-} 
+function dragLeave(event) {
+  event.preventDefault();
 
-function dragEnter(event){
-    event.preventDefault();
-       event.target.classList.add('over');
-   
-   }
-   
-   function dragLeave(event){
-       event.preventDefault();
-   
-    event.target.classList.remove('over');
-   }
+  event.target.classList.remove("over");
+}
 
-export default Chessboard; 
+export default Chessboard;
--- lol/src/index.js
+++ lol/src/index.js
@@ -1,19 +1,20 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Chessboard from './chessboard';
-import Header from './header';
+import { render } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import Chessboard from "./chessboard";
+import Header from "./header";
+import Footer from "./footer";
 
-class Home extends React.Component{
-    render(){
-        return(
-            <div>
-            <Header />
-            <Chessboard />
-            </div>
-        )
-    }
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Chessboard />
+        <Footer />
+      </div>
+    );
+  }
 }
 
-
-ReactDOM.render(<Home/ >,document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById("root"));
